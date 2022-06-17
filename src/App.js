341a84d1629fcc81d5359e2ee20d4ce6dd63a80b
Arpit <arpitbindal96@gmail.com>
2022-06-17 15:17:20 +0530

    nft landing page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import HomePage from "./pages/home/home";
 import { Routes, Route } from "react-router-dom";
 import AboutPage from "./pages/about/about";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import NFT from "./pages/NFT/NFT";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/about-us" element={<AboutPage />}></Route>
-      <Route path="/page-not-found" element={<PageNotFound />}></Route>
+      <Route path="*" element={<PageNotFound />}></Route>
+      <Route path="/nft" element={<NFT />}></Route>
     </Routes>
   );
 }
---
+++ src/containers/LandingPage-clients/LandingPageClient.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './LandingPageClient.css';
+
+function LandingPageClient() {
+    return (
+        <div className="LandingPageClient section__padding">
+
+            <div className="titleContainer">
+                <p className='gradient__text title__text'>Supported Chains</p>
+            </div>
+
+            <div className="iconsContainer">
+                <div className="icon">
+                    <img src="/BackedBy/NGC.png" alt="" />
+                </div>
+                <div className="icon">
+                    <img src="/BackedBy/NGC.png" alt="" />
+                </div>
+                <div className="icon">
+                    <img src="/BackedBy/NGC.png" alt="" />
+                </div>
+                <div className="icon">
+                    <img src="/BackedBy/NGC.png" alt="" />
+                </div>
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default LandingPageClient
