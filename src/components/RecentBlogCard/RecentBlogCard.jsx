2c03cc3509c8a1c4413f9a0f7b1e7a1797c4b349
Arpit <arpitbindal96@gmail.com>
2022-07-27 17:58:11 +0530

    change

--- src/components/RecentBlogCard/RecentBlogCard.jsx
+++ src/components/RecentBlogCard/RecentBlogCard.jsx
@@ -10,7 +10,7 @@ function RecentBlogCard({ title, blogs }) {
             <div className="RecentBlogCard__title">{title}</div>
             <div className="RecentBlogCard__listContainer">
                 {blogs.map((blog, index) => (
-                    <div className="blogCard ptr" key={index} onClick={() => { _navigate(`/view-blog/${blog?.id}`) }}>
+                    <div className="blogCard ptr" key={index} onClick={() => { _navigate(`/view-blog/${blog?.id}/${blog?.attributes?.title?.replaceAll(' ', '_')}`) }}>
                         <p className="title">{blog?.attributes?.title}</p>
                         <p className="date">
                             <MdUpdate />
